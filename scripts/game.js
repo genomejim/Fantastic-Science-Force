41ec6c486473959ddda7dbe61f8d4b518ca0daea
Genome Jim <gscjim@gmail.com>
2012-04-01 02:30:23 -0500

    added new areas and a truck and the base

--- scripts/game.js
+++ scripts/game.js
@@ -1,10 +1,18 @@
 game.fps = 50;
 var carx = 0;
 var cary = 400;
-var carv = 3;
+var carv = 5;
 var screen_name = 'road';
 var pause = 'false';
 var pause_time = 0;
+var play_intro = 'true';
+var jimx = 300;
+var jimy = 500;
+var jimv = - 2;
+var draw_jim = 'false';
+var truckx = 800;
+var trucky = 400;
+var truckv = -7;
 
 var _canvas = document.getElementById('game');
 var _canvasContext = null;
@@ -13,6 +21,10 @@ var img = new Image();
 img.src = "./content/images/car.png";
 var back = new Image();
 back.src = "./content/images/back_road.png";
+var jim = new Image();
+jim.src = "./content/images/jim.png";
+var truck = new Image();
+truck.src = "./content/images/truck.png";
 
 if (_canvas && _canvas.getContext) {
     _canvasContext = _canvas.getContext('2d');
@@ -27,23 +39,32 @@ _canvasBuffer.height = _canvas.height;
 
 var snd = new Audio("./content/sounds/intro.mp3");
 var snd_bye = new Audio("./content/sounds/bye.wav");
+var snd_traffic = new Audio("./content/sounds/traffic.wav");
 
 
 game.run = function () {
+    
     game.update();
     game.draw();
 
 }
 
 game.update = function() {
-
+    if (screen_name == 'road_3'){
+    truckx = truckx + truckv;
+    }
     carx = carx + carv;
-    snd.play();
+    if (play_intro == 'true') {
+	snd.play();
+	//snd_traffic.play();
+	play_intro = 'false';
+    }
     if (carx > 850 && screen_name == 'road') { 
         carx = 0;
         back.src = "./content/images/back_school.png";
         screen_name = 'school';
         pause_time = 0;
+	//snd_traffic.play();
      }
     if (carx == 600 && screen_name == 'school') {         
         carv = 0;
@@ -51,12 +72,12 @@ game.update = function() {
      }
     
     if (pause == 'true') {
-	if (pause_time == 0) {snd_bye.play();}
+	if (pause_time == 0 && screen_name == 'school') {snd_bye.play();}
         pause_time = pause_time + 1;
-        carx = 601;
+        if (screen_name == 'school') {carx = 601;}
     }
-    if (pause_time > 300) {
-        carv = 3;
+    if (pause_time > 150) {
+        carv = 5;
         pause = 'false';
     }
 
@@ -65,6 +86,7 @@ game.update = function() {
         cary = 450;
         back.src = "./content/images/back_road_2.png";
         screen_name = 'road_2';
+	//snd_traffic.play();
     }    
 
     if (carx > 850 && screen_name == 'road_2') { 
@@ -73,6 +95,7 @@ game.update = function() {
         back.src = "./content/images/back_road_3.png";
         screen_name = 'road_3';
         pause_time = 0;
+	snd_traffic.play();
      }    
 
     if (carx > 850 && screen_name == 'road_3') { 
@@ -81,10 +104,18 @@ game.update = function() {
         back.src = "./content/images/back_fsf_base.png";
         screen_name = 'base';
         pause_time = 0;
+	//snd_traffic.play();
      }    
-        if (carx == 600 && screen_name == 'base') {         
+        if (carx == 300 && screen_name == 'base') {
+	draw_jim = 'true';         
         carv = 0;
-        pause = 'true';        
+        pause = 'true'; 
+	snd.stop();
+	snd_traffic.play();
+	truckx = 800;	       
+     }
+     if (draw_jim == 'true') {
+	jimx = jimx + jimv;
      }
 
 }   
@@ -94,8 +125,13 @@ game.draw = function() {
         _canvasContext.clearRect(0,0,_canvas.width,_canvas.height);
         _canvasBufferContext.clearRect(0,0,_canvas.width,_canvas.height);
         _canvasBufferContext.drawImage(back, 0, 0);
+        if (draw_jim == 'true') {
+        _canvasBufferContext.drawImage(jim, jimx, jimy);	
+	}
+	_canvasBufferContext.drawImage(truck, truckx, trucky);
         _canvasBufferContext.drawImage(img, carx, cary);
         _canvasContext.drawImage(_canvasBuffer, 0 , 0);
+	
 }
 
 
