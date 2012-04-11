f40724e4882712b1419b54aa916d1a49e89ab746
Genome Jim <gscjim@gmail.com>
2012-04-11 06:57:36 -0500

    added seperate story script (just comments currently)

---
+++ scripts/story.js
@@ -0,0 +1,11 @@
+//jim drives
+//jim drops off max
+//jim drives and drives arrivng at FSF HQ
+//repeat at increasing pace
+//jim arrives at FSF HQ on story day one
+//jim get's tutorial text from npc
+//jim gets objective to talk to anniepennie
+//jim talks to anniepennie (who prefers annie) and gets save the lemur quest
+//jim battles ninjas and aliens as he traverses the lab to find the lemur
+//jim finds the lemur and is prompted to return to annie
+//jim returns to annie and recieves his first reward, the science beam
